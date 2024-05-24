af86fb53ba53c417115c185e38bac6548fdcd0fe
shovakei000 <shovakei000@warren.k12.in.us>
2024-05-24 10:19:01 -0400

    Cools

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.finalproject
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -24,39 +25,66 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun topLeftClick(view: View) {
-
+        val text: TextView = findViewById(R.id.topLeftButton) as TextView
+        text.setOnClickListener {
+            text.setText(getString(R.string.playerMove))
+        }
 
     }
     fun topMidClick(view: View) {
-
+        val text: TextView = findViewById(R.id.TopMiddlebutton) as TextView
+        text.setOnClickListener {
+            text.setText(getString(R.string.playerMove))
+        }
 
     }
     fun TopRightClick(view: View) {
-
+        val text: TextView = findViewById(R.id.topRightbutton) as TextView
+        text.setOnClickListener {
+            text.setText(getString(R.string.playerMove))
+        }
 
     }
     fun midLeftClick(view: View) {
-
+        val text: TextView = findViewById(R.id.midRightbutton) as TextView
+        text.setOnClickListener {
+            text.setText(getString(R.string.playerMove))
+        }
 
     }
     fun MiddleClick(view: View) {
-
+        val text: TextView = findViewById(R.id.Middlebutton) as TextView
+        text.setOnClickListener {
+            text.setText(getString(R.string.playerMove))
+        }
 
     }
     fun midRightClick(view: View) {
-
+        val text: TextView = findViewById(R.id.midRightbutton) as TextView
+        text.setOnClickListener {
+            text.setText(getString(R.string.playerMove))
+        }
 
     }
     fun botLeftClick(view: View) {
-
+        val text: TextView = findViewById(R.id.botLeftbutton) as TextView
+        text.setOnClickListener {
+            text.setText(getString(R.string.playerMove))
+        }
 
     }
     fun botMidClick(view: View) {
-
+        val text: TextView = findViewById(R.id.botMiddlebutton) as TextView
+        text.setOnClickListener {
+            text.setText(getString(R.string.playerMove))
+        }
 
     }
     fun botRightClick(view: View) {
-
+        val text: TextView = findViewById(R.id.botRightbutton) as TextView
+        text.setOnClickListener {
+            text.setText(getString(R.string.playerMove))
+        }
 
     }
 }
